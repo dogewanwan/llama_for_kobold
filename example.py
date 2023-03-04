acb05ddb63cb7aa8be8cb862cec3d7e85a9ea30e
Doge <dogeshibu@proton.me>
2023-03-04 13:25:28 +0200

    A port of OpenAI-like API to get kobold support

--- example.py
+++ example.py
@@ -8,24 +8,33 @@ import torch
 import fire
 import time
 import json
+import logging
+from datetime import datetime
 
+from flask import Flask, request
 from pathlib import Path
 
 from fairscale.nn.model_parallel.initialize import initialize_model_parallel
 
 from llama import ModelArgs, Transformer, Tokenizer, LLaMA
 
+app = Flask(__name__)
+
+@app.errorhandler(400)
+def handle_400_error(e):
+    app.logger.error(f"Bad request: {e}")
+    return "Bad request", 400
 
 def setup_model_parallel() -> Tuple[int, int]:
     local_rank = int(os.environ.get("LOCAL_RANK", -1))
     world_size = int(os.environ.get("WORLD_SIZE", -1))
 
-    torch.distributed.init_process_group("nccl")
+    torch.distributed.init_process_group("gloo")
     initialize_model_parallel(world_size)
     torch.cuda.set_device(local_rank)
 
     # seed must be the same in all processes
-    torch.manual_seed(1)
+    # torch.manual_seed(1)
     return local_rank, world_size
 
 
@@ -41,11 +50,11 @@ def load(ckpt_dir: str, tokenizer_path: str, local_rank: int, world_size: int) -
     with open(Path(ckpt_dir) / "params.json", "r") as f:
         params = json.loads(f.read())
 
-    model_args: ModelArgs = ModelArgs(max_seq_len=1024, max_batch_size=32, **params)
+    model_args: ModelArgs = ModelArgs(max_seq_len=2048, max_batch_size=1, **params)
     tokenizer = Tokenizer(model_path=tokenizer_path)
     model_args.vocab_size = tokenizer.n_words
     torch.set_default_tensor_type(torch.cuda.HalfTensor)
-    model = Transformer(model_args)
+    model = Transformer(model_args).cuda().half()
     torch.set_default_tensor_type(torch.FloatTensor)
     model.load_state_dict(checkpoint, strict=False)
 
@@ -53,20 +62,58 @@ def load(ckpt_dir: str, tokenizer_path: str, local_rank: int, world_size: int) -
     print(f"Loaded in {time.time() - start_time:.2f} seconds")
     return generator
 
+generator = None
 
-def main(ckpt_dir: str, tokenizer_path: str, temperature: float = 0.8, top_p: float = 0.95):
+@app.route("/v1/engines", methods=["GET"])
+def engines():
+    return { "data": [{"ready": True, "id": "llama"}] }
+
+@app.route("/v1/engines/llama/completions", methods=["POST"])
+def chat():
+    try:
+        # important for stable work on 16gb gpu
+        import gc
+        gc.collect()
+        try:
+            torch.cuda.empty_cache()
+        except:
+            pass
+
+        prompt = request.json.get("prompt", "Hello, world!")
+        max_tokens = request.json.get("max_tokens", 120)
+        temperature = request.json.get("temperature", 0.9)
+        
+        results = generator.generate([prompt], max_gen_len=max_tokens, temperature=temperature, top_p=0.95)[0].removeprefix(prompt)
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        results = ""
+
+    curr_dt = datetime.now()
+    return {
+        "id": "I don't need id, lmao",
+        "object": "text_completion",
+        "created": int(round(curr_dt.timestamp())),
+        "model": "llama-7B",
+        "choices": [
+            {
+                "text": results,
+                "index": 0,
+                "logprobs": None,
+                "finish_reason": "length"
+            }
+        ]
+    }
+
+def main(ckpt_dir: str = "../llama_model/7B", tokenizer_path: str = "../llama_model/tokenizer.model"):
     local_rank, world_size = setup_model_parallel()
     if local_rank > 0:
         sys.stdout = open(os.devnull, 'w')
 
+    global generator
     generator = load(ckpt_dir, tokenizer_path, local_rank, world_size)
-    prompts = ["The capital of Germany is the city of", "Here is my sonnet in the style of Shakespeare about an artificial intelligence:"]
-    results = generator.generate(prompts, max_gen_len=256, temperature=temperature, top_p=top_p)
 
-    for result in results:
-        print(result)
-        print("\n==================================\n")
-
+    app.run(host="0.0.0.0", port=1234)
 
 if __name__ == "__main__":
     fire.Fire(main)
--- llama/generation.py
+++ llama/generation.py
@@ -8,7 +8,6 @@ import torch
 from llama.tokenizer import Tokenizer
 from llama.model import Transformer
 
-
 class LLaMA:
     def __init__(self, model: Transformer, tokenizer: Tokenizer):
         self.model = model
@@ -39,10 +38,18 @@ class LLaMA:
         start_pos = min_prompt_size
         prev_pos = 0
         for cur_pos in range(start_pos, total_len):
-            logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
+            input_ids = tokens[:, prev_pos:cur_pos]
+            logits = self.model.forward(input_ids, prev_pos)
             if temperature > 0:
-                probs = torch.softmax(logits / temperature, dim=-1)
-                next_token = sample_top_p(probs, top_p)
+
+                next_token_scores = sample_top_p_actual(input_ids, logits, top_p)
+                next_token_scores = sample_tail_free(input_ids, next_token_scores, 1.0)
+                next_token_scores = sample_typical(input_ids, next_token_scores, 1.0)
+                next_token_scores = sample_temperature(input_ids, next_token_scores, temperature)
+                next_token_scores = sample_advanced_repetition_penalty(input_ids, next_token_scores, 1024, 0.7, 1.1)
+
+                next_token_scores = torch.nn.functional.softmax(next_token_scores, dim=-1)
+                next_token = torch.multinomial(next_token_scores, num_samples=1).squeeze(1)
             else:
                 next_token = torch.argmax(logits, dim=-1)
             next_token = next_token.reshape(-1)
@@ -65,13 +72,108 @@ class LLaMA:
             decoded.append(self.tokenizer.decode(t))
         return decoded
 
+# taken from Kobold and transformers so the code below may be AGPL I guess, so be wary of that fact
+def sample_temperature(input_ids, scores, tempt):
+    scores = scores / tempt
+    return scores
 
-def sample_top_p(probs, p):
-    probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
-    probs_sum = torch.cumsum(probs_sort, dim=-1)
-    mask = probs_sum - probs_sort > p
-    probs_sort[mask] = 0.0
-    probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
-    next_token = torch.multinomial(probs_sort, num_samples=1)
-    next_token = torch.gather(probs_idx, -1, next_token)
-    return next_token
+def sample_typical(input_ids, scores, typical, filter_value = -float("Inf"), min_tokens_to_keep = 1):
+    if filter_value >= 1.0:
+        return scores
+
+    probs = scores.softmax(dim=-1)
+    log_probs = probs.log()
+
+    neg_entropy = (probs * log_probs).nansum(dim=-1, keepdim=True)
+
+    entropy_deviation = (neg_entropy - log_probs).abs()
+
+    _, sorted_indices = torch.sort(entropy_deviation)
+    sorted_logits = probs.gather(-1, sorted_indices)
+    sorted_indices_to_remove = sorted_logits.cumsum(dim=-1) >= typical
+    sorted_indices_to_remove = sorted_indices_to_remove.roll(1, dims=-1)
+
+    min_tokens_to_keep = max(min_tokens_to_keep, 1)
+    sorted_indices_to_remove[..., : min_tokens_to_keep] = 0
+
+    indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+    scores = scores.masked_fill(indices_to_remove, filter_value)
+    return scores    
+
+def sample_top_p_actual(input_ids, scores, top_p, filter_value = -float("Inf"), min_tokens_to_keep = 1):
+    sorted_logits, sorted_indices = torch.sort(scores, descending=False)
+    cumulative_probs = sorted_logits.softmax(dim=-1).cumsum(dim=-1)
+
+    sorted_indices_to_remove = cumulative_probs <= (1 - top_p)
+    if min_tokens_to_keep > 1:
+        sorted_indices_to_remove[..., -min_tokens_to_keep :] = 0
+
+    indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+    scores = scores.masked_fill(indices_to_remove, filter_value)
+    return scores
+
+def sample_advanced_repetition_penalty(input_ids, scores, penalty_range, penalty_slope, penalty):
+    penalty_range = int(penalty_range)
+    clipped_penalty_range = min(input_ids.shape[-1], penalty_range)
+
+    if penalty != 1.0:
+        if penalty_range > 0:
+            if clipped_penalty_range < input_ids.shape[1]:
+                input_ids = input_ids[..., -clipped_penalty_range:]
+
+            if penalty_slope != 0:
+                _penalty = (torch.arange(penalty_range, dtype=scores.dtype, device=scores.device)/(penalty_range - 1)) * 2. - 1
+                _penalty = (penalty_slope * _penalty) / (1 + torch.abs(_penalty) * (penalty_slope - 1))
+                _penalty = 1 + ((_penalty + 1) / 2).unsqueeze(0) * (penalty - 1)
+                penalty = _penalty[..., -clipped_penalty_range:]
+
+        score = torch.gather(scores, 1, input_ids)
+        score = torch.where(score <= 0, score * penalty, score / penalty)
+        scores.scatter_(1, input_ids, score)
+
+        return scores    
+
+def sample_top_a(input_ids, scores, top_a, filter_value = -float("Inf"), min_tokens_to_keep = 1):
+    if filter_value >= 1.0:
+        return scores
+
+    sorted_logits, sorted_indices = torch.sort(scores, descending=True)
+    probs = sorted_logits.softmax(dim=-1)
+
+    probs_max = probs[..., 0, None]
+    sorted_indices_to_remove = probs < probs_max * probs_max * top_a
+
+    if min_tokens_to_keep > 1:
+        sorted_indices_to_remove[..., : min_tokens_to_keep] = 0
+
+    indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+    scores = scores.masked_fill(indices_to_remove, filter_value)
+    return scores    
+
+def sample_tail_free(input_ids, scores, tfs, filter_value = -float("Inf"), min_tokens_to_keep = 1):
+    if filter_value >= 1.0:
+        return scores
+    sorted_logits, sorted_indices = torch.sort(scores, descending=True)
+    probs = sorted_logits.softmax(dim=-1)
+
+    d2 = probs.diff().diff().abs()
+    normalized_d2 = d2 / d2.sum(dim=-1, keepdim=True)
+    normalized_d2_cdf = normalized_d2.cumsum(dim=-1)
+
+    sorted_indices_to_remove = normalized_d2_cdf > tfs
+
+    sorted_indices_to_remove = torch.cat(
+        (
+            torch.zeros(scores.shape[0], 1, dtype=torch.bool, device=scores.device),
+            sorted_indices_to_remove,
+            torch.ones(scores.shape[0], 1, dtype=torch.bool, device=scores.device),
+        ),
+        dim=-1,
+    )
+
+    if min_tokens_to_keep > 1:
+        sorted_indices_to_remove[..., : min_tokens_to_keep] = 0
+
+    indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
+    scores = scores.masked_fill(indices_to_remove, filter_value)
+    return scores    
